f5ba2bd8b2e5fe93b4ce3316ddff0fcfe7b5e884
Filip Gawlik <filippo.gawlik@gmail.com>
2024-02-15 18:02:46 +0100

    Added average

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ Resources:
 
 from sympy import *
 from math import sqrt, pi
+from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
 import sys
 
 sys.setrecursionlimit(10000)
 
-SAVES_IMAGES = False  # Save images
+MODE = "average"  # Modes: coherent, average, squezzed
+SAVE_IMAGES = True  # Save images
 PHI_COUNT = 20  # Count of phi functions (N)
 ALPHA = 1  # Alpha
-T_STEP = 0.05  # Time step
+T_STEP = 0.25  # Time step
 
 x = Symbol("x")  # Variable x
 phis = []  # Phi list
+avgs = []  # Averages of coherent states
 
 
 def fact_sqrt(n):
@@ -42,6 +45,14 @@ def normalize_wave_function(phi):
     return phi / sqrt(integrate(abs(phi) ** 2, (x, -oo, oo)))
 
 
+def calculate_average_of_wave_function(func):
+    """
+    Average of wave function
+    """
+
+    return re(integrate(x * abs(func) ** 2, (x, -oo, oo)).evalf())
+
+
 def calculate_phis():
     global phis
 
@@ -59,8 +70,6 @@ def calculate_phis():
         phis.append(last_phi)
 
         print(f"phi_{i + 1} Calculated!")
-
-    return phis
 
 
 def calculate_coherent(t):
@@ -82,32 +91,54 @@ def calculate_coherent(t):
 
 
 def plot_state(t):
-    state = calculate_coherent(t)
-    graph_re = re(state)
-    graph_im = im(state)
-    graph_abs2 = abs(state) ** 2
-
     id = int(t * 100)
 
     print(f"Plotting state: id={id}...")
 
-    tmp_plt = plotting.plot(
-        graph_re, graph_im, xlim=[-10, 10], ylim=[-1, 1], show=(not SAVES_IMAGES)
-    )
+    if MODE == "coherent":
+        state = calculate_coherent(t)
+        graph_re = re(state)
+        graph_im = im(state)
+        graph_abs2 = abs(state) ** 2
 
-    if SAVES_IMAGES:
-        tmp_plt.save(f"./states_re_im/coherent_t_{id}.png")
+        tmp_plt = plotting.plot(
+            graph_re, graph_im, xlim=[-10, 10], ylim=[-1, 1], show=(not SAVE_IMAGES)
+        )
 
-    plt.close()
+        if SAVE_IMAGES:
+            tmp_plt.save(f"./states_re_im/coherent_t_{id}.png")
 
-    tmp_plt = plotting.plot(
-        graph_abs2, xlim=[-10, 10], ylim=[-1, 1], show=(not SAVES_IMAGES)
-    )
+        plt.close()
 
-    if SAVES_IMAGES:
-        tmp_plt.save(f"./states_abs2/coherent_t_{id}.png")
+        tmp_plt = plotting.plot(
+            graph_abs2, xlim=[-10, 10], ylim=[-1, 1], show=(not SAVE_IMAGES)
+        )
 
-    plt.close()
+        if SAVE_IMAGES:
+            tmp_plt.save(f"./states_abs2/coherent_t_{id}.png")
+
+        plt.close()
+    elif MODE == "average":
+        state = calculate_coherent(t)
+        avg = calculate_average_of_wave_function(state)
+
+        avgs.append(avg)
+
+        c = Circle((avg, 0), 0.2)
+        figure, axes = plt.subplots()
+        axes.set_xlim([-3, 3])
+        axes.set_ylim([-2, 2])
+        axes.set_aspect(1)
+        axes.add_artist(c)
+
+        if SAVE_IMAGES:
+            plt.savefig(f"./states_avg/coherent_t_{id}.png")
+        else:
+            plt.show()
+
+        plt.close()
+    elif MODE == "squezzed":
+        pass
 
     print("Successfully plotted state!")
 
@@ -117,6 +148,16 @@ def main():
 
     for i in range(0, int(4 * pi / T_STEP) + 1):
         plot_state(T_STEP * i)
+
+    if MODE == "average":
+        a = 0
+
+        for x in avgs:
+            a += x
+
+        a /= len(avgs)
+
+        print(f"Total average is: {a}")
 
 
 main()
